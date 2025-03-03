799ca6b9df0a8f29b1b8abf588ea92b267071a4f
MoonBasedKas <kassidy.maberry@gmail.com>
2025-03-03 16:25:06 -0700

    basic latexifying

--- tools/scripts/csvToLatex.js
+++ tools/scripts/csvToLatex.js
@@ -1,5 +1,60 @@
+alignment = " c "
 
 function parseTable(){
     text = document.getElementById("csvInput").value
+
+    lines = text.split("\n")
+    lines = splitLines(lines)
+    width = findTableWidth(lines)
+    console.log(lines)
+
+    text = latexify(lines)
+
     document.getElementById("latexOutput").innerHTML = text
+}
+
+function splitLines(lines){
+    for(i = 0; i < lines.length; i++){
+        lines[i] = lines[i].split(",");
+    }
+    return lines
+}
+
+function findTableWidth(lines){
+    max = 0
+    for(i = 0; i < lines.length; i++){
+        if(lines[i].length > max){
+            max = lines[i].length
+        }
+    }
+    return max
+}
+
+
+function latexify(lines, width){
+    text = "\\begin{tabular}{"
+    for(i = 0; i < width; i++){
+        text += alignment
+    }
+    text += "}<br>"
+
+    for(i = 0; i < lines.length; i++){
+        for(j = 0; j < lines[i].length; j++){
+
+            text += lines[i][j]
+
+            if (j != lines[i].length - 1){
+                text += "&"
+            }    
+            
+        }
+        
+        if (i != lines[i].length - 1){
+            text += "\\\\<br>"
+        }
+    }
+
+    text += "\\end{tabular}"
+
+    return text
 }
